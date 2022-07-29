a016f7c5f8ff6b547f4ba18c29bf487680b2e341
aiswarya <aiswaryavalsan@gmail.com>
2022-07-29 10:13:00 -0500

    lastElementChild

--- demo/demo.js
+++ demo/demo.js
@@ -4,3 +4,8 @@ var itemlist1=document.querySelector("#items");
 console.log(itemlist1.parentElement);
 itemlist1.parentElement.style.backgroundColor='gray';
 console.log(itemlist1.parentElement.parentElement.parentElement);
+console.log(itemlist1.childNodes);
+console.log(itemlist1.children);
+console.log(itemlist1.children[1]);
+console.log(itemlist1.lastChild);
+itemlist1.lastElementChild.style.color='red';
