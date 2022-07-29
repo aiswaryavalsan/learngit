fb00cf049cbe7425f6005c2153832e389dddc8d7
aiswarya <aiswaryavalsan@gmail.com>
2022-07-29 10:04:32 -0500

    parentElement

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,6 @@
-var itemlist=document.querySelector("#items");
-console.log(itemlist.parentElementNode);
-itemlist.parentNode.style.backgroundColor='gray';
+
+//console.log(itemlist.parentNode.parentNode.parentNode);
+var itemlist1=document.querySelector("#items");
+console.log(itemlist1.parentElement);
+itemlist1.parentElement.style.backgroundColor='gray';
+console.log(itemlist1.parentElement.parentElement.parentElement);
