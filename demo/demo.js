ca19a0bee658f1ce98c369e6052929111ccb6841
aiswarya <aiswaryavalsan@gmail.com>
2022-07-29 09:57:37 -0500

    parentnode

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,3 @@
-var item4=document.querySelectorAll('li:nth-child(odd)');
-for(let i=0;i<item4.length;i++)
- item4[i].style.backgroundColor='green';
-console.log(item4);
-var item2=document.querySelectorAll('li');
-item2[1].style.color='green';
+var itemlist=document.querySelector("#items");
+console.log(itemlist.parentElementNode);
+itemlist.parentNode.style.backgroundColor='gray';
