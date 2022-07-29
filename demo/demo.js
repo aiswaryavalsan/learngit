485d157c35b4965d437204773663d5d9d83ed936
aiswarya <aiswaryavalsan@gmail.com>
2022-07-29 04:50:03 -0500

    querselectorall

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,6 @@
-var item1=document.querySelector('li:nth-child(2)');
-item1.style.backgroundColor='green';
-var item3=document.querySelector('.list-group-item:nth-child(3');
-item3.style.display='none';
-
+var item4=document.querySelectorAll('li:nth-child(odd)');
+for(let i=0;i<item4.length;i++)
+ item4[i].style.backgroundColor='green';
+console.log(item4);
+var item2=document.querySelectorAll('li');
+item2[1].style.color='green';
