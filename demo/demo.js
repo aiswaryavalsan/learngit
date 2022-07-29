7a11dff818ca4fedcaccb9369f39b2d6c23befbb
aiswarya <aiswaryavalsan@gmail.com>
2022-07-29 04:02:10 -0500

    third commit

--- demo/demo.js
+++ demo/demo.js
@@ -1,25 +1,16 @@
-//console.log(document.domain);
-//console.log(document.URL);
-//console.log(document.title);
-///document.title="change title";
-////console.log(document.title);
-//console.log(document.doctype);
-//console.log(document.head);
-//var headertext=document.getElementById('header-title');
-//console.log(headertext);
-///headertext.innerText="hello";
-//headertext.textContent="hai";
-//headertext.innerHTML='<h3>hello all</h3>';
-var header=document.getElementById('title1');
-header.style.color='green';
-header.style.fontWeight='bold';
-console.log(header);
-var headertitle=document.getElementById('header-title');
-headertitle.style.border='solid 3px black';
-//document.write(header.text);
+
+
 var item=document.getElementsByClassName('list-group-item');
 for(let i=0;i<item.length;i++)
 {
     item[i].style.fontWeight='bold';
 }
 item[2].style.backgroundColor="green";
+
+var items=document.getElementsByTagName('li');
+console.log(items);
+items[4].style.color="red";
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor='yellowgreen';
+}
