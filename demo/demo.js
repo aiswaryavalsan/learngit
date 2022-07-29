906e085e533065b64ee49125651bd1d333276921
aiswarya <aiswaryavalsan@gmail.com>
2022-07-29 12:49:02 -0500

    create

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,13 @@
-var item=document.querySelector("#items");
-console.log(item.firstChild);
-item.lastElementChild.textContent="hello";
-console.log(item.nextSibling);
-console.log(item.previousSibling);
-console.log(item.nextElementSibling);
-console.log(item.previousElementSibling);
-item.previousElementSibling.style.color='green';
+var newdiv=document.querySelector('div');
+newdiv.className='helo1';
+newdiv.id='helo';
+var newdivtext=document.createTextNode("hello");
+var container=document.querySelector('container1');
+console.log(container);
+var h1=document.querySelector('header h1');
+console.log(h1);
+newdiv.appendChild(newdivtext);
+//container.insertBefore(newdiv,h1);
+console.log(newdivtext);
+
+
