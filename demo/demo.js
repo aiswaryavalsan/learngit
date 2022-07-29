e4670db088c5694ea256d969252d3b4d2237d9b7
aiswarya <aiswaryavalsan@gmail.com>
2022-07-29 10:19:27 -0500

    firstchild

--- demo/demo.js
+++ demo/demo.js
@@ -1,12 +1,2 @@
-
-//console.log(itemlist.parentNode.parentNode.parentNode);
-var itemlist1=document.querySelector("#items");
-console.log(itemlist1.parentElement);
-itemlist1.parentElement.style.backgroundColor='gray';
-console.log(itemlist1.parentElement.parentElement.parentElement);
-console.log(itemlist1.childNodes);
-console.log(itemlist1.children);
-console.log(itemlist1.children[1]);
-console.log(itemlist1.lastChild);
-itemlist1.lastElementChild.style.color='red';
-itemlist1.firstElementChild.style.backgroundColor="yellowgreen";
+var item=document.querySelector("#items");
+console.log(item.firstChild);
