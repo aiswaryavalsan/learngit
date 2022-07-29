b5cf141e32d1eda38fc5ecdd858f9c4cb150d21a
aiswarya <aiswaryavalsan@gmail.com>
2022-07-29 10:29:46 -0500

    sibling

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,8 @@
 var item=document.querySelector("#items");
 console.log(item.firstChild);
-
+item.lastElementChild.textContent="hello";
+console.log(item.nextSibling);
+console.log(item.previousSibling);
+console.log(item.nextElementSibling);
+console.log(item.previousElementSibling);
+item.previousElementSibling.style.color='green';
